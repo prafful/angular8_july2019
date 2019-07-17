744534c228a0b587b93e485f001403452ee0eec8
Prafful Daga <prraful@gmail.com>
2019-07-17 11:21:44 +0530

    httpClient post and put

--- src/app/consume/remote-employee/remote-employee.component.ts
+++ src/app/consume/remote-employee/remote-employee.component.ts
@@ -11,6 +11,12 @@ export class RemoteEmployeeComponent implements OnInit {
   allEmployees:any
   selectedEmployee: any
   selected = 0
+  newEmployee:any = {}
+  myname=""
+  myage = 0
+  mysalary = 0
+  myid = 0
+
   constructor(private empService:EmployeeServiceService) { }
 
   getAllEmployees = function(){
@@ -41,5 +47,61 @@ export class RemoteEmployeeComponent implements OnInit {
     })
     //this.getAllEmployees()
   }
+
+  addEmployee = function(tempEmployee){
+    console.log(tempEmployee.value);
+    this.newEmployee  = tempEmployee.value
+    /* this.newEmployee = {
+                  "name":"praful",
+                  "salary":"1234",
+                  "age":"88"
+                }
+ */    this.empService.createEmployee(this.newEmployee).subscribe((response)=>{
+      console.log(response);
+      this.getAllEmployees()
+    })
+  }
+
+  updateEmployee = function(selectedEmployee){
+    this.selected = this.allEmployees[selectedEmployee].id
+    this.empService.getEmployeeById(this.selected).subscribe((response)=>{
+      console.log(response);
+      this.myname = response.employee_name
+      this.myage = response.employee_age
+      this.mysalary = response.employee_salary
+      this.myid = response.id
+    })
+  }
+
+  updateEmployeeNow = function(){
+   this.newEmployee = {
+                  "name":this.myname,
+                  "salary":this.mysalary,
+                  "age":this.myage
+                }
+      console.log(this.newEmployee);          
+      this.empService.updateEmployeeById(this.myid, this.newEmployee).subscribe((response)=>{
+        console.log(response);
+        this.getAllEmployees()
+      })          
+
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/app/service/employee-service.service.ts
+++ src/app/service/employee-service.service.ts
@@ -17,16 +17,16 @@ export class EmployeeServiceService {
     return this.http.get("http://dummy.restapiexample.com/api/v1/employee/" + employeeId)
   }
 
-  updateEmployeeById(){
-
+  updateEmployeeById(id, employee){
+    return this.http.put("	http://dummy.restapiexample.com/api/v1/update/"+id, employee )
   }
 
   deleteEmployeeById(employeeId){
     return this.http.delete("http://dummy.restapiexample.com/api/v1/delete/" + employeeId)
   }
 
-  createEmployee(){
-
+  createEmployee(employee){
+    return this.http.post("http://dummy.restapiexample.com/api/v1/create", employee)
   }
 
 
